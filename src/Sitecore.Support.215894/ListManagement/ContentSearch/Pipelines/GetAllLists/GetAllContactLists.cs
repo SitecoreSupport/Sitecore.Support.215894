6ac068ca945d0c49b9a0255e634b2c0db65c8599
DK\anr <anr@sitecore.net>
2018-04-12 17:21:20 +0300

    refactor

--- src/Sitecore.Support.215894/ListManagement/ContentSearch/Pipelines/GetAllLists/GetAllContactLists.cs
+++ src/Sitecore.Support.215894/ListManagement/ContentSearch/Pipelines/GetAllLists/GetAllContactLists.cs
@@ -1,41 +1,38 @@
-﻿using Sitecore.ContentSearch;
-using Sitecore.Data;
-using Sitecore.Data.Items;
-using Sitecore.Diagnostics;
-using Sitecore.ListManagement.Configuration;
-using Sitecore.ListManagement.ContentSearch;
-using Sitecore.ListManagement.ContentSearch.Model;
-using Sitecore.ListManagement.ContentSearch.Pipelines;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Sitecore.Support.ListManagement.ContentSearch.Pipelines.GetAllLists
+﻿namespace Sitecore.Support.ListManagement.ContentSearch.Pipelines.GetAllLists
 {
-  public class GetAllContactLists : ListProcessor
-  {
-    private readonly ISearchIndex index;
-    public GetAllContactLists()
-    {
-      this.index = ContentSearchManager.GetIndex(ListManagementSettings.ContactListIndexName);
-    }
+    using Sitecore.ContentSearch;
+    using Sitecore.Data;
+    using Sitecore.Data.Items;
+    using Sitecore.Diagnostics;
+    using Sitecore.ListManagement.Configuration;
+    using Sitecore.ListManagement.ContentSearch;
+    using Sitecore.ListManagement.ContentSearch.Model;
+    using Sitecore.ListManagement.ContentSearch.Pipelines;
+    using System.Linq;
 
-    public virtual void Process(ListsArgs args)
+    public class GetAllContactLists : ListProcessor
     {
-      Assert.ArgumentNotNull(args, "args");
-      ID itemId = (args.RootId != (ID)null) ? args.RootId : base.RootId;
-      Item item = base.Database.GetItem(itemId);
-      if (item == null)
-      {
-        args.ResultLists = Enumerable.Empty<ContactList>().AsQueryable<ContactList>();
-      }
-      else
-      {
-        ID[] templateIds = new ID[] { base.TemplateId, base.SegmentedListTemplateId };
-        args.ResultLists = new QueryableProxy<ContactList>(new Sitecore.Support.ListManagement.ContentSearch.ContactListQueryProvider(this.index, item.ID, item.ID, templateIds, args.IncludeSubFolders));
-      }
-    }
+        private readonly ISearchIndex index;
+        public GetAllContactLists()
+        {
+            this.index = ContentSearchManager.GetIndex(ListManagementSettings.ContactListIndexName);
+        }
 
-  }
+        public virtual void Process(ListsArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            ID itemId = (args.RootId != (ID)null) ? args.RootId : base.RootId;
+            Item item = base.Database.GetItem(itemId);
+            if (item == null)
+            {
+                args.ResultLists = Enumerable.Empty<ContactList>().AsQueryable<ContactList>();
+            }
+            else
+            {
+                ID[] templateIds = new ID[] { base.TemplateId, base.SegmentedListTemplateId };
+                args.ResultLists = new QueryableProxy<ContactList>(new Sitecore.Support.ListManagement.ContentSearch.ContactListQueryProvider(this.index, item.ID, item.ID, templateIds, args.IncludeSubFolders));
+            }
+        }
+
+    }
 }
