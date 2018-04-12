6b0bc358a2dba47e7727084a94aa1604ae25232b
DK\anr <anr@sitecore.net>
2018-04-12 17:08:58 +0300

    import code

---
+++ src/Sitecore.Support.215894/ListManagement/ContentSearch/Pipelines/GetAllLists/GetAllContactLists.cs
@@ -0,0 +1,41 @@
+﻿using Sitecore.ContentSearch;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.ListManagement.Configuration;
+using Sitecore.ListManagement.ContentSearch;
+using Sitecore.ListManagement.ContentSearch.Model;
+using Sitecore.ListManagement.ContentSearch.Pipelines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.ListManagement.ContentSearch.Pipelines.GetAllLists
+{
+  public class GetAllContactLists : ListProcessor
+  {
+    private readonly ISearchIndex index;
+    public GetAllContactLists()
+    {
+      this.index = ContentSearchManager.GetIndex(ListManagementSettings.ContactListIndexName);
+    }
+
+    public virtual void Process(ListsArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+      ID itemId = (args.RootId != (ID)null) ? args.RootId : base.RootId;
+      Item item = base.Database.GetItem(itemId);
+      if (item == null)
+      {
+        args.ResultLists = Enumerable.Empty<ContactList>().AsQueryable<ContactList>();
+      }
+      else
+      {
+        ID[] templateIds = new ID[] { base.TemplateId, base.SegmentedListTemplateId };
+        args.ResultLists = new QueryableProxy<ContactList>(new ContactListQueryProvider(this.index, item.ID, item.ID, templateIds, args.IncludeSubFolders));
+      }
+    }
+
+  }
+}
---
+++ src/Sitecore.Support.215894/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.215894")]
+[assembly: AssemblyProduct("Sitecore.Support.215894")]
+[assembly: ComVisible(false)]
